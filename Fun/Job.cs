94afecb514c6f63343b855b32a6e91b0d7feac96
Luiz Fernando Nunes Marques <luizfnunesmarques@gmail.com>
2016-04-25 23:56:40 -0300

    Hack start.

---
+++ Fun/Job.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fun
+{
+    class Job
+    {
+        public Job()
+        {
+
+        }
+
+        public string nome { get; internal set; }
+        public int numero { get; set; }
+
+        public void Executar()
+        {
+            Console.WriteLine("Running prime numbero of: " + numero + " PT: "+Thread.CurrentThread.IsThreadPoolThread + " ID: " + Thread.CurrentThread.ManagedThreadId);
+          
+            Fatorial();
+        }
+
+
+        private void Fatorial()
+        {            
+            Console.WriteLine("First prime number" + numero + " " + FindPrimeNumber(numero));
+        }
+
+        public long FindPrimeNumber(int n)
+        {
+            int count = 0;
+            long a = 2;
+            while (count < n)
+            {
+                long b = 2;
+                int prime = 1;// to check if found a prime
+                while (b * b <= a)
+                {
+                    if (a % b == 0)
+                    {
+                        prime = 0;
+                        break;
+                    }
+                    b++;
+                }
+                if (prime > 0)
+                    count++;
+                a++;
+            }
+            return (--a);
+        }
+
+        private async Task<string> RunHttpJob()
+        {
+            WebClient webClient = new WebClient();
+            var tasklist = new List<Task<string>>();
+            return await webClient.DownloadStringTaskAsync("http://" + nome);              
+        }
+    }
+}
---
+++ Fun/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Job> lista = new List<Job>();
+            lista.Add(new Job() { nome = "google.com",   numero = 50001 });
+            lista.Add(new Job() { nome = "yahoo.com",    numero = 50002 });
+            lista.Add(new Job() { nome = "facebook.com", numero = 50003 });
+            lista.Add(new Job() { nome = "google.com",   numero = 50004 });
+            lista.Add(new Job() { nome = "google.com", numero = 50005 });
+            lista.Add(new Job() { nome = "google.com", numero = 50006 });
+            lista.Add(new Job() { nome = "google.com", numero = 50007 });
+            lista.Add(new Job() { nome = "google.com", numero = 50008 });
+            lista.Add(new Job() { nome = "google.com", numero = 50009 });
+            lista.Add(new Job() { nome = "google.com", numero = 50010 });
+            lista.Add(new Job() { nome = "google.com", numero = 50011 });
+            lista.Add(new Job() { nome = "google.com", numero = 50012 });
+            lista.Add(new Job() { nome = "google.com", numero = 50013 });
+            lista.Add(new Job() { nome = "google.com", numero = 50014 });
+            lista.Add(new Job() { nome = "google.com", numero = 50015 });
+
+
+            Console.WriteLine("MainThread " + Thread.CurrentThread.IsThreadPoolThread + " " + Thread.CurrentThread.ManagedThreadId);
+            Parallel.ForEach(lista, new ParallelOptions() { MaxDegreeOfParallelism = 4 } ,(a) => a.Executar());
+            Console.ReadKey();               
+
+        }
+
+        
+    }
+}
+
